be378fc8fc80972c2133c277ef69502d7b148f2c
WijnandVanDijk <60653370+WijnandVanDijk@users.noreply.github.com>
2020-12-11 21:34:26 +0100

    iets verder gegaan met agent, volgens nagel model

--- Opdracht 4 auto's/Code/Agent.py
+++ Opdracht 4 auto's/Code/Agent.py
@@ -1,9 +1,14 @@
-import mesa
+from mesa import Agent
+import random
 
 class CarAgent(Agent):
 
-    snelheid = 100
-    max_snelheid = 100
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.snelheid = 4
+
+    snelheid = 5
+    max_snelheid = 5
 
     def auto(self):
         self.snelheid = CarAgent.snelheid
@@ -22,5 +27,11 @@ class CarAgent(Agent):
     def move(self):
         pass
 
-    def Role_of_randomization(self)
-        pass:
+    def role_of_randomization(self):
+        if self.snelheid > 1:
+            if random.randint(0,1) == 1:
+                self.snelheid -= 1
+            else:
+                self.snelheid = self.snelheid
+
+        return self.snelheid
